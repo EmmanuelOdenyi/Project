d3b825d800c195803145f89f35cb164eb98ad339
EmmanuelOdenyi <86023622+EmmanuelOdenyi@users.noreply.github.com>
2021-11-18 19:00:47 -0800

    Add files via upload

---
+++ Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+
+
+{
+   [Range(1, 10)]
+public float jumpVelocity;
+public Rigidbody2D rb;
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            rb.velocity = Vector3.up * jumpVelocity;
+
+        }
+    }
+}
